2e8f06302d4738e9fb328fa76fc7581e7db11d98
Umar Ishaq <umarishaq26@gmail.com>
2026-05-22 10:09:50 +0100

    build(deps-dev): add gh-pages package for deployment to GitHub Pages
    Add gh-pages as a devDependency to enable deploying the app to GitHub Pages. The package-lock.json was updated accordingly to include gh-pages and its transitive dependencies.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Click Counter</h1>
+      <main>
+        <p className='info'>Click the button to increase the count:</p>
+        <button onClick={handleClick} className='btn'>Click me!</button>
+        <p className='count'>Count: {count}</p>
+      </main>
     </div>
   );
 }
